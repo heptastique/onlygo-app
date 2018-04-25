524214ae1043e142f9736fccab801b754c92d102
Vincent Redouté <vincent.redoute@live.fr>
2018-04-25 17:33:33 +0200

    thumb reacts to evaluation note

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,8 @@ import {AlertController, NavController} from 'ionic-angular';
 import { User } from "../../entities/user";
 import { UserService } from "../../services/user.service";
 import {ActivityCreationPage} from '../activity-creation/activity-creation';
+import {EvaluationService} from '../../services/evaluation.service';
+import {Evaluation} from '../../entities/evaluation';
 
 @Component({
   selector: 'page-home',
@@ -11,9 +13,9 @@ import {ActivityCreationPage} from '../activity-creation/activity-creation';
 
 export class HomePage {
 
-  color =  'yellow';
-
+  color =  'black';
   thumbup =  true;
+  evaluation: Evaluation;
 
   loadProgress = 50;
 
@@ -25,10 +27,35 @@ export class HomePage {
     email: ""
   };
 
-  constructor(public alertCtrl: AlertController, private userService: UserService, private navCtrl: NavController) { }
+  constructor(public alertCtrl: AlertController, private userService: UserService, private navCtrl: NavController,
+              private evaluationService: EvaluationService) { }
 
   ionViewDidLoad () {
     this.getUser();
+    this.getEvaluation();
+  }
+
+  getEvaluation(){
+    this.evaluationService.getEvaluationNow().subscribe(
+      evaluation => {
+        this.evaluation = evaluation;
+        console.log(this.evaluation.note);
+        if(this.evaluation.note<0.5){
+          this.thumbup = false;
+          this.color='red';
+        }else{
+          this.thumbup = true;
+          this.color='green';
+        }},
+      (err) => {
+        console.error(err);
+        let alert = this.alertCtrl.create({
+          title: 'La requête a échoué.',
+          subTitle: 'Vous devez être authentifié pour accéder à cette ressource.',
+          buttons: ['OK']
+        });
+        alert.present();
+      });
   }
 
   getUser(): void {
