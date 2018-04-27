3928047d0b2bb0d34f508dc0e835777411ab5e67
Vincent Redouté <vincent.redoute@live.fr>
2018-04-27 10:32:59 +0200

    map in start activity

--- src/pages/activity/activity.ts
+++ src/pages/activity/activity.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {  NavController,  } from 'ionic-angular';
 import {ActivityCreationPage} from '../activity-creation/activity-creation';
 import {GeolocationService} from '../../services/geolocation.service';
 import {Gps_Coordinates} from '../../entities/gps_coordinates';
 
+declare var google;
 
 @Component({
   selector: 'page-activity',
@@ -11,6 +12,9 @@ import {Gps_Coordinates} from '../../entities/gps_coordinates';
 })
 
 export class ActivityPage {
+
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
 
   activityStarted = false;
 
@@ -25,6 +29,7 @@ export class ActivityPage {
   startActivity(){
     this.activityStarted = true;
     this.coordsLog = [];
+    this.loadMap();
     this.geolocationService.startRecording(1000);
   }
 
@@ -32,9 +37,49 @@ export class ActivityPage {
     this.activityStarted = false;
     this.geolocationService.stopRecording();
     this.coordsLog = this.geolocationService.getListCoord();
+    this.map = null;
   }
 
   addActivity() {
     this.navCtrl.push(ActivityCreationPage);
+  }
+
+  loadMap(){
+    this.geolocationService.getPos().then((coords) =>
+    {
+      let latLng = new google.maps.LatLng(coords.x, coords.y);
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+      this.addCurrentPoint();
+    });
+  }
+
+  addCurrentPoint(){
+    let marker = new google.maps.Marker({
+      map: this.map,
+      position: this.map.getCenter(),
+      animation: google.maps.Animation.DROP
+    });
+
+    let content = "<h4>Ma localisation</h4>";
+
+    this.addInfoWindow(marker, content);
+  }
+
+  addInfoWindow(marker, content){
+
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
   }
 }
