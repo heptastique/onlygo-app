49b1ee9998a3f676489178f4e8069b455e1dd999
Vincent Redouté <vincent.redoute@live.fr>
2018-04-27 11:07:15 +0200

    creating activity details page

---
+++ src/pages/activity-details/activity-details.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-activity-details',
+  templateUrl: 'activity-details.html',
+})
+export class ActivityDetailsPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+  }
+
+}
