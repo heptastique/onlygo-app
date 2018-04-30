00aee25889fbb931b27cb71d12ba83bc14aa1bdb
Vincent Redouté <vincent.redoute@live.fr>
2018-04-30 23:58:29 +0200

    removing useless content

--- src/pages/preferences/preferences.ts
+++ src/pages/preferences/preferences.ts
@@ -5,8 +5,6 @@ import { LoginService } from '../../services/login.service';
 import { UserService } from '../../services/user.service';
 import { AuthService } from '../../services/auth.service';
 import { User } from '../../entities/user';
-import {Gps_Coordinates} from '../../entities/gps_coordinates';
-import {GeolocationService} from '../../services/geolocation.service';
 import {ProgrammeService} from '../../services/programme.service';
 import {LocationModalPage} from '../location-modal/location-modal';
 
@@ -30,8 +28,7 @@ export class PreferencesPage {
 
   constructor(public navCtrl: NavController, private loginService: LoginService, private userService: UserService,
               private authService: AuthService, public appCtrl: App, public alertCtrl : AlertController,
-              private geolocationService: GeolocationService, private programmeService: ProgrammeService,
-              public modalCtrl: ModalController) { }
+              private programmeService: ProgrammeService, public modalCtrl: ModalController) { }
 
   ionViewDidLoad () {
     this.getUser();
@@ -109,58 +106,6 @@ export class PreferencesPage {
 
   generateProgramme(){
     this.programmeService.generateProgramme().subscribe(() => {});
-  }
-
-  promptLocation(coords: Gps_Coordinates, title: string){
-    let alert = this.alertCtrl.create({
-      title: title,
-      inputs: [
-        {
-          name: 'x',
-          type: 'number',
-          min: '0',
-          value: coords.x.toString()
-        },
-        {
-          name: 'y',
-          type: 'number',
-          min: '0',
-          value: coords.y.toString()
-        }
-      ],
-      buttons: [
-        {
-          text: 'Annuler',
-          role: 'cancel',
-        },
-        {
-          text: 'Valider',
-          handler: data => {
-            let coor : Gps_Coordinates = {
-              x: data.x,
-              y: data.y
-            }
-            this.userService.updateLocation(coor).subscribe(
-              (res) => { this.user.location = res.location },
-              (err) => {
-                let message;
-                if(err.status == 0) {
-                  message = 'Impossible de contacter le serveur. Veuillez vérifier votre connexion.';
-                }else{
-                  message = err.error;
-                }
-                let alert = this.alertCtrl.create({
-                  title: 'Erreur lors de la mise à jour.',
-                  subTitle: message,
-                  buttons: ['OK']
-                });
-                alert.present();
-              });
-          }
-        }
-      ]
-    });
-    alert.present();
   }
 
   logout() {
