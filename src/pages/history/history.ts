6199f10dd907b98359ce61f9aa6d0805908cc3a0
Vincent Redouté <vincent.redoute@live.fr>
2018-04-30 20:05:15 +0200

    display activities done

--- src/pages/history/history.ts
+++ src/pages/history/history.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {AlertController, NavController} from 'ionic-angular';
 import {DateService} from '../../services/date.service';
+import {ProgrammeService} from '../../services/programme.service';
+import {Programme} from '../../entities/programme';
+import {Activity} from '../../entities/activity';
 
 @Component({
   selector: 'page-history',
@@ -16,7 +19,16 @@ export class HistoryPage {
   };
 
 
-  constructor(public navCtrl: NavController, private dateService: DateService) { }
+  activites: Activity [];
+
+  programme : Programme = {
+    activites: this.activites,
+    user: null
+  };
+
+
+  constructor(public navCtrl: NavController, private dateService: DateService, private programmeService: ProgrammeService,
+              public alertCtrl: AlertController) { }
 
   ionViewDidLoad(){
     this.displayCurrentWeek();
@@ -24,13 +36,36 @@ export class HistoryPage {
 
   displayCurrentWeek(){
     this.week = this.dateService.getCurrentWeek();
+    this.getProgrammeByDate(this.week.date);
   }
 
   displayNextWeek(){
     this.week = this.dateService.getNextWeek(this.week.date);
+    this.getProgrammeByDate(this.week.date);
   }
 
   displayLastWeek(){
     this.week = this.dateService.getLastWeek(this.week.date);
+    this.getProgrammeByDate(this.week.date);
+  }
+
+  getProgrammeByDate(date: Date){
+    this.programmeService.getProgrammeByDate(date).subscribe(
+      (programme) => {
+        this.programme=programme;
+        for(let i in this.programme.activites){
+          this.programme.activites[i].date = this.dateService.getDateFromString(this.programme.activites[i].date);
+          console.log(this.programme.activites[i].date);
+        }
+      },
+      (err) => {
+        console.error(err);
+        let alert = this.alertCtrl.create({
+          title: 'La requête a échoué.',
+          subTitle: 'Vous devez être authentifié pour accéder à cette ressource.',
+          buttons: ['OK']
+        });
+        alert.present();
+      });
   }
 }
