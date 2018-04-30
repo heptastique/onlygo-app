8c9600ef82e928eb9afc6ed0a491a3991e02714b
Vincent Redouté <vincent.redoute@live.fr>
2018-04-30 22:05:39 +0200

    location modal

---
+++ src/pages/location-modal/location-modal.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-location-modal',
+  templateUrl: 'location-modal.html',
+})
+export class LocationModalPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+  }
+
+}
