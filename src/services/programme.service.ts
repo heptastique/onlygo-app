2f8233d53af31ea552e9e132d3f3e89286c671c8
Vincent Redouté <vincent.redoute@live.fr>
2018-04-30 20:04:57 +0200

    fix, post needed

--- src/services/programme.service.ts
+++ src/services/programme.service.ts
@@ -1,8 +1,12 @@
 import {API_SERVER} from '../app/app.constants';
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import {Programme} from '../entities/programme';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable()
 export class ProgrammeService {
@@ -17,6 +21,6 @@ export class ProgrammeService {
 
   getProgrammeByDate(date: Date):Observable<Programme>{
     const url = `${API_SERVER.programme}/getbydate`;
-    return this.http.get<Programme>(url);
+    return this.http.post<Programme>(url, {'dateDebut' : date}, httpOptions);
   }
 }
