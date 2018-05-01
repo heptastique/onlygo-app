4e43f5e691f45a5ac167a429a9fb3eb653bb0932
Victor <vb4007@gmail.com>
2018-05-01 23:11:29 +0200

    New connection page

--- src/pages/registration/registration.ts
+++ src/pages/registration/registration.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import {AlertController, NavController, NavParams} from 'ionic-angular';
+import {AlertController, NavController, NavParams, ToastController, LoadingController} from 'ionic-angular';
 import {User} from "../../entities/user";
 import {UserService} from "../../services/user.service";
+import { GeolocationService } from '../../services/geolocation.service';
 
 @Component({
   selector: 'page-registration',
@@ -17,37 +18,53 @@ export class RegistrationPage {
     firstname: "",
     lastname: "",
     email: "",
-    objectifHebdo: 0,
-    distanceMax: 5,
+    objectifHebdo: null,
+    distanceMax: null,
     location: null
   };
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private userService: UserService,
-              public alertCtrl: AlertController) { }
+              public alertCtrl: AlertController, private geolocationService: GeolocationService,
+              public toastCtrl: ToastController, public loadingCtrl: LoadingController) { }
 
   ionViewDidLoad() { }
 
-  register(){
+  async register(){
+    let loading = this.loadingCtrl.create({
+      content: 'Inscription en cours...'
+    });
+    loading.present();
     if(this.user.password != this.passwordVerification){
       let alert = this.alertCtrl.create({
         title: 'Les mots de passes ne sont pas identiques.',
         subTitle: '',
         buttons: ['OK']
       });
+      loading.dismiss();
       alert.present();
       return;
     }
-    this.userService.addUser(this.user).subscribe((user) => {
+    await this.geolocationService.getPos().then(location => {
+      return new Promise(resolve => {
+        this.user.location = location;
+        resolve();
+      })
+    })
+    await this.userService.addUser(this.user).subscribe((user) => {
       this.user = user;
-        let alert = this.alertCtrl.create({
-          title: 'Registration success!',
-          buttons: ['OK']
-        });
-        alert.present();
+      let toast = this.toastCtrl.create({
+        message: 'Inscription réussie ! Vous pouvez vous connecter',
+        duration: 3000,
+        showCloseButton: true,
+        closeButtonText: 'Ok'
+      });
+      loading.dismiss();
+      toast.present();
       this.navCtrl.pop();
     },
       (err) =>{
         console.error(err);
+        loading.dismiss();
         let message;
         if(err.status == 0) {
           message = 'Impossible de contacter le serveur. Veuillez vérifier votre connexion.';
