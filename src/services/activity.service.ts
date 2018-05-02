5ce32edfb439cb7d45b0e5640a2145036dd7bfb3
Vincent Redouté <vincent.redoute@live.fr>
2018-05-02 22:22:22 +0200

    new service

--- src/services/activity.service.ts
+++ src/services/activity.service.ts
@@ -1,9 +1,13 @@
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {API_SERVER} from '../app/app.constants';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {Activity} from '../entities/activity';
+import {Gps_Coordinates} from '../entities/gps_coordinates';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable()
 export class ActivityService{
@@ -13,5 +17,11 @@ export class ActivityService{
   getNextPlanned(): Observable<Activity>{
     const url = `${API_SERVER.activity}/nextPlanned`;
     return this.http.get<Activity>(url);
+  }
+
+  getItanary(idActivity: number): Observable<Gps_Coordinates []>{
+    const url = `${API_SERVER.activity}/itenary`;
+    let params = new HttpParams().set("activite",idActivity.toString());
+    return this.http.get<Gps_Coordinates []>(url, {headers: httpOptions.headers, params: params});
   }
 }
