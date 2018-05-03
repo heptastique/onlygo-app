69f5549cbe1dfa0bf80d414c7ae9b3796221a1b1
Vincent Redouté <vincent.redoute@live.fr>
2018-05-03 17:02:12 +0200

    activity service

--- src/services/activity.service.ts
+++ src/services/activity.service.ts
@@ -14,11 +14,20 @@ export class ActivityService{
 
   constructor(private http: HttpClient) {}
 
+  /**
+   * Get the next activity of the user
+   * @returns {Observable<Activity>}
+   */
   getNextPlanned(): Observable<Activity>{
     const url = `${API_SERVER.activity}/nextPlanned`;
     return this.http.get<Activity>(url);
   }
 
+  /**
+   * Get the path of the next activity of the user
+   * @param {number} idActivity
+   * @returns {Observable<Gps_Coordinates[]>}
+   */
   getItenary(idActivity: number): Observable<Gps_Coordinates []>{
     const url = `${API_SERVER.activity}/itinary`;
     let params = new HttpParams().set("activite",idActivity.toString());
